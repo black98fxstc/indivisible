abf1c08726a9583261705ff68891f0c6cc794c70
wmoore <20290182+black98fxstc@users.noreply.github.com>
2019-05-02 19:34:33 -0700

    remove keys from twilio

--- docker/make.sh
+++ docker/make.sh
@@ -8,4 +8,7 @@ sed --file ../KEYS.sed config.php.no-keys >config.php
 cd ../isf-wiki
 chmod go+r KEYS.php
 chmod go+r LocalSettings.php
+cd ../twilio
+sed --file ../KEYS.sed env.no-keys >.env
 cd ..
+echo "don't for get 2FA"
